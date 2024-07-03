6258c6ce742eb223a3133f8023534936ca61755c
Yuri Ppo <yuri@yurippo.com>
2024-07-03 10:10:35 -0400

    New Comments 2024-07-03

---
+++ cypress/e2e/qa-tools.cy.js
@@ -0,0 +1,9 @@
+// No nosso Projeto nos concentramos em ensinar conceitos independentemente das ferramentas. Porém, existem várias ferramentas que podem nos auxiliar na hora da criação de casos de testes, reporte de bugs, métricas, etc. Agora, vamos entender sobre algumas delas:
+
+// TestLink - https://www.testlink.org/
+//  é uma ferramenta open source de gerenciamento de testes. Com ela conseguimos cadastrar projetos de teste, bem como seus respectivos planos e casos de testes. É possível controlar e armazenar a execução dos casos especificados e também gerar um relatório dos testes que já foram executados. Além disso, permite a integração com diversas ferramentas de rastreamento de defeitos como Jira, Mantis, Bugzilla, dentre outras.
+
+// Bugzilla e Mantis são duas ferramentas populares para o rastreamento de bugs. Ambas são de código aberto e oferecem recursos como reporte de bugs, relatórios, gráficos, além da possibilidade de definição de prioridades de bugs envolvendo clientes e opções como mandar e-mails quando um bug é reportado ou resolvido, por exemplo.
+
+// Jira tem interação com ferramentas da Microsoft e é muito utilizada pelas empresas, servindo para a gestão de todo o projeto e suas atividades. Oferece suporte para qualquer metodologia ágil, como Scrum e Kanban. Com ela é possível acompanhar as tarefas sendo desenvolvidas, reportar e acompanhar bugs. Por meio de um plugin chamado XRay, ela se torna uma ferramenta poderosa para a gestão, permitindo planejar, projetar e executar testes, bem como gerar relatórios.
+
